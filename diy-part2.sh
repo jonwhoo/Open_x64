083f47f7e16dced7b307b000f660aaebc2f84ef9
jonwhoo <107388668+jonwhoo@users.noreply.github.com>
2026-08-19 16:11:58 +0800

    Update default IP and hostname in diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.1.70/g' package/base-files/files/bin/config_generate
+# 1. 修改默认后台 IP（如改为 192.168.10.1，不改可删掉此行）
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# 2. 修改默认主机名为 OpenWrt-x86
+sed -i 's/OpenWrt/CuiWrt-x86/g' package/base-files/files/bin/config_generate
