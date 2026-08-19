a3da4f403ec9a68bc45a64e3081824ef435f60f4
jonwhoo <107388668+jonwhoo@users.noreply.github.com>
2026-08-19 17:57:54 +0800

    Update comments and modify default IP and hostname

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# 1. 修改默认后台 IP（如改为 192.168.10.1，不改可删掉此行）
+# 进入 openwrt 源码目录（最关键的一行，防止找不到文件）
+cd openwrt
+
+# 1. 修改默认后台 IP（改为 192.168.10.1）
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# 2. 修改默认主机名为 OpenWrt-x86
+# 2. 修改默认主机名为 CuiWrt-x86
 sed -i 's/OpenWrt/CuiWrt-x86/g' package/base-files/files/bin/config_generate
