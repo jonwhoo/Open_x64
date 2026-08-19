26d385463908e82c0f283068b8bd7a6d8fc69b51
jonwhoo <107388668+jonwhoo@users.noreply.github.com>
2026-08-19 19:28:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 进入 openwrt 源码目录（最关键的一行，防止找不到文件）
+# 进入 openwrt 源码目录
 cd openwrt
 
 # 1. 修改默认后台 IP（改为 192.168.10.1）
@@ -8,3 +8,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 
 # 2. 修改默认主机名为 CuiWrt-x86
 sed -i 's/OpenWrt/CuiWrt-x86/g' package/base-files/files/bin/config_generate
+
+# 3. 删除源码自带的旧版 PassWall 避免与外部源冲突
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf package/lean/luci-app-passwall
